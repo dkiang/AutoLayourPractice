c3ce1bc153293ba3f64786a279b39dcb8a9913b6
dkiang <douglas.kiang@gmail.com>
2022-11-01 07:49:41 -0700

    Initial commit

---
+++ 2.11-AutoLayoutPractice/2.11-AutoLayoutPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2.11-AutoLayoutPractice
+//
+//  Created by Douglas Kiang on 11/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
